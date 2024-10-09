da798391321cb702b2edb7bc8b5cd4fcf458c4c5
VanyaMark <markova.vanya@gmail.com>
2024-10-09 19:08:01 +0200

    working

--- scoring-words.js
+++ scoring-words.js
@@ -10,7 +10,7 @@ function high(x) {
     // PRIMERO: Darle un valor númerico a cada letra. 
     //Se me ocurre hacer un array con las letras donde su valor sea su posición en el index +1
 
-    const letters = ['a', , 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     // Al intentar poner esto dentro de una variable descubro metodo bind(). VER DESPUES
 
@@ -25,7 +25,9 @@ function high(x) {
 
     // Recorrer cada palabra (array) para poner comparar cuál tiene más puntos
     for (let w of arrayPhrase) {
-
+        if(w.includes('a') && w.includes('e') && w.includes('i') && w.includes('o') && w.includes('u')){
+            return w
+        }
         //contar los puntos de cada palabra iterando dentro de la misma  
         let pointstWord = 0;
         for (let i of w) {
